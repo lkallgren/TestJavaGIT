4541b69c4901deca0442468d75890996fd55d2b1
User <User@7GZ7J32>
2016-11-11 16:32:52 +0100

    first commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-	  System.out.println("Hej");
+
+            // Testing updating GIT
+            System.out.println("Hej");
 
 	}
 
